994170ee286cc0a7ad7c665288e2fe889a3eeabd
Aaron Whitfield <aawhitfield@hotmail.com>
2021-10-03 21:00:13 -0700

    added some of the constants for the watch and getting health factor history data

--- lib/smartwatch_api.dart
+++ lib/smartwatch_api.dart
@@ -1,7 +1,22 @@
 library smartwatch_api;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+
+class SmartWatchAPI {
+
+  static const String smartWatchNameV1 = '1963YH';
+  static const String smartWatchNameV2 = '2025';
+
+  static const List<int> heartRateHistoryData = [0x55, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x55 + 0) & 0xff];
+  static const List<int> eraseHeartRateHistoryData = [0x55, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x55 + 0x99) & 0xff];
+  static const List<int> bloodPressureHistoryData = [0x56, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x56 +0 & 0xff)];
+  static const List<int> eraseBloodPressureHistoryData = [0x56, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x56 + 0x99) & 0xff];
+  static const List<int> bloodOxygenHistoryData = [0x60, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x60 +0 & 0xff)];
+  static const List<int> eraseBloodOxygenHistoryData = [0x60, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x60 + 0x99) & 0xff];
+  static const List<int> bodyTemperatureHistoryData = [0x62, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x62 +0 & 0xff)];
+  static const List<int> eraseBodyTemperatureHistoryData = [0x62, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x62 + 0x99) & 0xff];
+  static const List<int> stepsHistoryData = [0x51, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x51 + 0) & 0xff];
+  static const List<int> eraseStepsHistoryData = [0x51, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x51 + 0x99) & 0xff];
+  static const List<int> sleepHistoryData = [0x53, 0x00, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x53 + 0) & 0xff];
+  static const List<int> eraseSleepHistoryData = [0x53, 0x99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, (0x53 + 0x99) & 0xff];
+
 }
